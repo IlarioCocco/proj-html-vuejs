6d9ff8dbd3f5c86ac395c896349fc3b4ba28766e
IlarioCOCCO <ilario.cocco@gmail.com>
2021-10-12 12:59:27 +0200

    img commit debug

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="Header">
+      <img src="./assets/img/av-freelance-logo-retina.png" alt="">
+        
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+   
+
+</style>
