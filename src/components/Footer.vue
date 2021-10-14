e0c3ec33c9f891eff9a4bdc0f3a114ef380d0d44
IlarioCOCCO <ilario.cocco@gmail.com>
2021-10-14 15:47:18 +0200

    debug commit footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="Footer">
-    <div class="boxFooter">
-      <h1 class="text-center">footer</h1>
+
+    <div class="boxFooter d-flex justify-content-center">
+      <img  class="logoAv" src="../assets/img/avada-footer-logo-66x66.png" alt="logo av">
+    </div>
+
+    <div class="boxFooter d-flex justify-content-center">
+      <h6 class="if">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        amet consectetur adipisicing elit.
+      </h6>
+    </div>
+
+    <div class="boxFooterIcon d-flex justify-content-center">
+      <img  class="logoAvSmall" src="../assets/img/avada-footer-logo-66x66.png" alt="logo av">
+      <img  class="logoAvSmall" src="../assets/img/avada-footer-logo-66x66.png" alt="logo av">
+      <img  class="logoAvSmall" src="../assets/img/avada-footer-logo-66x66.png" alt="logo av">
+      <img  class="logoAvSmall" src="../assets/img/avada-footer-logo-66x66.png" alt="logo av">
     </div>
    
   </div>
@@ -27,11 +41,22 @@ export default {
 <style scoped lang="scss">
 
 .boxFooter {
-  padding: 50px;
+  padding-top: 50px;
   background: #191c23;
   color: white;
-
 }
   
+
+.logoAvSmall {
+  width: 25px;
+  margin: 5px;
+}
+
+.boxFooterIcon {
+  padding-top: 10px;
+  background: #191c23;
+  color: white;
+  padding-bottom: 50px;
+}
 
 </style>
